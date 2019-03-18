3370bf75a7156f3ffe85cfd1fa0606bd87e54e55
BantuD <500069424@stu.upes.ac.in>
2019-03-18 12:40:21 +0530

    Sum of 2 & 5 code added in the program file:

---
+++ Program.cpp
@@ -0,0 +1,63 @@
+//Creating a Virtual calculator for 2 and 5 with help of classes:
+#include<iostream>
+using namespace std;
+class Sum{
+
+        public:
+                int i,k;
+        void getdata(int x,int y){
+                i = x;
+                k = y;
+        }
+        int putdata(){
+		return (i+k);
+	}
+};
+int main(){
+
+	Sum s;
+	s.getdata(2,5);
+	cout<<"The sum of 2 and 5 is: "<<s.putdata();
+	return 0;
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+Program.cpp[+] [unix] (05:29 01/01/1970)                                                   18,10-31 All
+-- INSERT --
+
